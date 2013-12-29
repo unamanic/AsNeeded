35dcd725154e9aff08f79dd80ac7aa7cb5b1f090
William Witt <william@witt-family.net>
2013-12-29 14:16:21 -0600

    Initial Commit

---
+++ MedMinder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MedMinder
+//
+//  Created by William Witt on 12/29/13.
+//  Copyright (c) 2013 William Witt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MedMinderAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MedMinderAppDelegate class]));
+    }
+}
